59ec63299593942fa5ff36a0fb650f81ed542204
Jacob Miner <zyko101@gmail.com>
2014-02-01 20:19:09 -0800

    Added code to parse command file
    Also added commands.ini file.
    Working on sending all output to client.
    Added a few comments

--- Server.py
+++ Server.py
@@ -1,35 +1,57 @@
 import socket
+import sys
 import thread
 import os
+import subprocess
+commands = {}
 
+"""
+	Get all the commands from the ini file
+"""
+def populateCommands():
+	with open("commands.ini") as f:
+		for line in f:
+			(usrCommand, svrCommand) = line.split(":", 1)
+			commands[usrCommand] = svrCommand;
+
+"""
+	Attempt to process the command. If command doesn't exist, don't blow up.
+"""
 def processCommand(command):
-	if command == "MAC":
-		print "staring mac"
-		os.system('"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm OSX')
-	elif command == "KALI":
-		print "staring kali"
-		os.system('"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm kali')		
+	try:
+		exec commands[command]
+	except KeyError:
+		print "Command does not exist!"
 
+"""
+	Get commands from the client.
+"""
 def getCommands(client):
 	command = ''
 	while command != "EXIT":
-		print "waiting for data"
 		command = client.recv(1024)
-		processCommand(command)
-	print "exiting"
+		processCommand(command, client)
 	client.close()
 
-listenPort 		= 63555
-HOST 			= "localhost"
+"""
+	Start the server, bind the address
+"""
+def startServer():
+	populateCommands()
 
-listenSocket 	= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listenSocket.bind((HOST, listenPort))
-listenSocket.listen(1)
+	PORT = 63555
+	HOST = "localhost"
 
-while True:
-	client, address = listenSocket.accept()
-	print 'Connecion Address: ', address
-	thread.start_new_thread(getCommands, (client,))
+	listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	listenSocket.bind((HOST, PORT))
+	listenSocket.listen(1)
 
-listenSocket.close()
+	while True:
+		client, address = listenSocket.accept()
+		print 'Connecion Address: ', address
+		sys.stdout = client
+		thread.start_new_thread(getCommands, (client,))
+	listenSocket.close()
 
+startServer()
